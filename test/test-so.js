ddb276761dc86bd9e8e403035b65c02eec716eb1
pauloferreira25 <paulo.ferreira25@gmail.com>
2017-01-10 17:20:22 -0200

    CPU SO General

--- test/test-so.js
+++ test/test-so.js
@@ -1,5 +1,9 @@
 'use strict';
-
+const Datastore = require('nedb');
+const path = require('path');
+const dirname = path.dirname(__filename);
+const dBconfig = new Datastore(dirname + '/config.db');
+dBconfig.loadDatabase();
 const core = require('../index.js');
 
 describe('mSOPlatform', function() {
@@ -8,6 +12,19 @@ describe('mSOPlatform', function() {
         data['moduleFunction'] = "mOSPlatform";
         core.run(data).then(result => {
             console.log(result);
+            done();
+        }).catch(error => {
+            done(error);
+        });
+    });
+});
+
+describe('mOSCPU', function() {
+    it('get mOSCPU', function(done) {
+        let data = {};
+        data['moduleFunction'] = "mOSCPU";
+        core.run(data, dBconfig).then(result => {
+            console.log(JSON.stringify(result));
             done();
         }).catch(error => {
             done(error);
